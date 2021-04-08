4335716f7d61c964c2e632fa47c535dd8d623233
Arpan Sharma <arpan.sharma26@gmail.com>
2021-04-08 13:19:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const hbs = require('hbs');
+hbs.registerPartials('./views/partials');
+
+const pubPath = path.join(__dirname, './public');
+app.set('view engine', 'hbs');
+
+app.get('/aboutus', (req, res) => {
+	let date = new Date();
+	res.render('aboutus', {
+		todayDate: date,
+		dateD: date.getDate(),
+		dateM: date.getMonth() + 1,
+		dateY: date.getFullYear(),
+	});
+});
+
+app.get('/appointments', (req, res) => {
+	res.render('appointments', {});
+});
+
+app.get('/ourservices', (req, res) => {
+	res.render('ourservices', {});
+});
+
+app.get('/ourbarbers', (req, res) => {
+	res.render('ourbarbers', {});
+});
+
+app.get('/contact', (req, res) => {
+	res.render('contact', {});
+});
+
+app.listen(PORT, () => {
+	console.log('Server has been started.');
+});
+
+app.use(express.static(pubPath));
